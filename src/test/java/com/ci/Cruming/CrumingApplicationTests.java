51cf7064e86413d1fe3e6672bd8a46e95d0a9987
ddangme <ssilver121@naver.com>
2024-10-28 11:29:52 +0900

    chore: 프로젝트 초기 설정

---
+++ src/test/java/com/ci/Cruming/CrumingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ci.Cruming;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrumingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
