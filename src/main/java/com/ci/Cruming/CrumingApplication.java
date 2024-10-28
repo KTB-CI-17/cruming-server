c423d295726cb8dc6b41ec27a6c5b89c9aba88a5
ddangme <ssilver121@naver.com>
2024-10-28 11:29:52 +0900

    chore: 프로젝트 초기 설정

---
+++ src/main/java/com/ci/Cruming/CrumingApplication.java
@@ -0,0 +1,13 @@
+package com.ci.Cruming;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrumingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrumingApplication.class, args);
+	}
+
+}
