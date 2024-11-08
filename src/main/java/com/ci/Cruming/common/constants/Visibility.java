00804d38048fc260f162cef9c4d143e700bda190
ddangme <ssilver121@naver.com>
2024-11-08 15:59:32 +0900

    feat: 게시글 등록
    회원 정보 조회 기능 제외 추후 연동 필요

---
+++ src/main/java/com/ci/Cruming/common/constants/Visibility.java
@@ -0,0 +1,7 @@
+package com.ci.Cruming.common.constants;
+
+public enum Visibility {
+    PUBLIC,
+    FOLLOWER,
+    PRIVATE
+}
---
+++ src/main/java/com/ci/Cruming/shoes/entity/Shoes.java
@@ -0,0 +1,31 @@
+package com.ci.Cruming.shoes.entity;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "shoes")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Shoes {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 200)
+    private String photo;
+
+    @Column(nullable = false, length = 300)
+    private String korean;
+
+    @Column(nullable = false, length = 100)
+    private String english;
+
+    @Column(nullable = false, length = 500)
+    private String link;
+
+    @Column(length = 600)
+    private String tip;
+}
