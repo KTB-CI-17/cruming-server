23a46483c65a2cedb8554a2f18a48c2a7b8a10d8
ddangme <ssilver121@naver.com>
2024-11-12 23:27:21 +0900

    style: 중복 코드 제거 및 경로 수정

--- src/main/java/com/ci/Cruming/user/repository/UserRepository.java
+++ src/main/java/com/ci/Cruming/user/repository/UserRepository.java
@@ -1,6 +1,6 @@
 package com.ci.Cruming.user.repository;
 
-import com.ci.Cruming.user.entity.Platform;
+import com.ci.Cruming.common.constants.Platform;
 import com.ci.Cruming.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
