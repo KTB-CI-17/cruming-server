2fcd392e90c4493f013384ba9bf693b88f92586c
ddangme <ssilver121@naver.com>
2024-12-04 13:05:53 +0900

    docs: 게시글 작성 dto 스웨거 문서 작성

--- src/main/java/com/ci/Cruming/file/dto/FileRequest.java
+++ src/main/java/com/ci/Cruming/file/dto/FileRequest.java
@@ -1,7 +1,15 @@
 package com.ci.Cruming.file.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
+@Schema(description = "파일 업로드 요청 DTO")
 public record FileRequest(
+        @Schema(description = "원본 파일명",
+                example = "climbing_problem.jpg")
         String originalFileName,
+
+        @Schema(description = "파일 표시 순서",
+                example = "1")
         Integer displayOrder
 ) {
 }
