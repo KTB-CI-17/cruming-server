0fdcfcfb533e3db8a249293eb26d29a4c562a703
hyungjulim <hjlim4u@naver.com>
2024-12-17 17:15:59 +0900

    timeline날짜별 조회 메소드 추가

--- src/main/java/com/ci/Cruming/timeline/dto/TimelineListResponse.java
+++ src/main/java/com/ci/Cruming/timeline/dto/TimelineListResponse.java
@@ -1,6 +1,8 @@
 package com.ci.Cruming.timeline.dto;
 
 import java.time.LocalDateTime;
+
+import com.ci.Cruming.location.dto.LocationDTO;
 import com.ci.Cruming.location.entity.Location;
 import com.ci.Cruming.timeline.entity.Timeline;
 import com.ci.Cruming.user.entity.User;
@@ -13,7 +15,7 @@ public record TimelineListResponse(
     Long id,
     String content,
     String level,
-    Location location,
+    LocationDTO location,
     LocalDateTime createdAt,
     Long userId,
     String userNickname,
@@ -25,7 +27,8 @@ public record TimelineListResponse(
             .id(timeline.getId())
             .content(timeline.getContent())
             .level(timeline.getLevel())
-            .location(timeline.getLocation())
+            .location(timeline.getLocation() != null ? 
+                     LocationDTO.fromEntity(timeline.getLocation()) : null)
             .createdAt(timeline.getCreatedAt())
             .userId(timeline.getUser().getId())
             .userNickname(timeline.getUser().getNickname())
