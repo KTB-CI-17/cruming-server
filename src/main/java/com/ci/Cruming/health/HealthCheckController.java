ea6abc2397b405f9e85e9587dfd7cc0b582b1996
ddangme <ssilver121@naver.com>
2024-12-24 01:00:58 +0900

    test: db connection test

--- src/main/java/com/ci/Cruming/health/HealthCheckController.java
+++ src/main/java/com/ci/Cruming/health/HealthCheckController.java
@@ -1,17 +1,24 @@
 package com.ci.Cruming.health;
 
+import com.ci.Cruming.location.dto.LocationRequest;
+import com.ci.Cruming.location.entity.Location;
+import com.ci.Cruming.location.service.LocationService;
+import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.HashMap;
 import java.util.Map;
 
+@RequiredArgsConstructor
 @RestController
 public class HealthCheckController {
 
 	private static final Logger logger = LoggerFactory.getLogger(HealthCheckController.class);
+	private final LocationService locationService;
 
 	@GetMapping("/api/v1/health")
 	public Map<String, String> health() {
@@ -23,4 +30,12 @@ public class HealthCheckController {
 		logger.info("Health check data: {}", returnMap);
 		return returnMap;
 	}
+
+	@GetMapping("/api/v1/dbtest")
+	 public ResponseEntity<Location> dbtest() {
+		LocationRequest request = new LocationRequest("테스트용", "서울시 강남구 우리집", 12.23434, 23.3123124);
+
+		return ResponseEntity.ok(locationService.getOrCreateLocation(request));
+	}
+
 }
